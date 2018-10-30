0e3c57605e4a6a7123d479bd30899df24676901d
AfeeqaAzdi <44602310+AfeeqaAzdi@users.noreply.github.com>
2018-10-30 16:53:14 +0800

    update hello world

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,13 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
+        String a = "Hello";
+        String b = "World";
         // Prints "Hello, World" to the terminal window.
         //Hacktoberfest is best
         //btw hello world
         System.out.println("Hello, World");
+        System.out.println(a + " " +b);
     }
 
 }
