733789188bb598d0d2cd8e4439e775a5bb7b3eca
AfeeqaAzdi <44602310+AfeeqaAzdi@users.noreply.github.com>
2018-10-30 17:00:42 +0800

    create HelloWorldHacktober

---
+++ HelloWorldHacktober.java
@@ -0,0 +1,7 @@
+public class HelloWorldHacktober {
+    public static void main(String[] args) {
+    
+        System.out.println("Hello World");
+        System.out.println("Hi Hacktoberfest!");
+    }
+}
